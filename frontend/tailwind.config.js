3d2df7da071fabe87a568e9ad74883cc53003c79
PatilAnshul <anshulp027@gmail.com>
2024-12-15 02:05:08 +0530

    servicesCarousel

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}' // Includes all source files for scanning Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
