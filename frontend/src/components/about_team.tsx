0ca3d6363f59d33284087486bc926e5887b90b69
sneha824 <sneharjagtap82@gmail.com>
2025-01-04 06:41:04 +0530

    Added data to testimonial(also made changes in code),Team member,About 4 card(3done)

--- frontend/src/components/about_team.tsx
+++ frontend/src/components/about_team.tsx
@@ -3,8 +3,23 @@ import Photo from "./about_photo"; // Adjust the import path if necessary
 
 const Team: React.FC = () => {
   const teamMember = {
-    name: "John Doe",
-    description: "John is a passionate software engineer with expertise in frontend development and a love for creating seamless user experiences.",
+    name: (
+      <>
+        Girija ,<br />
+        Connect-Ed Founder & Lead Counsellor
+        <br/>
+      </>
+    ),
+    description:(
+      <>
+        <p>A passionate College and Careers Guidance Counsellor with more than 7 years of experience in the evolving field of education, Girija loves equipping students with key habits and skills to craft a meaningful future. She believes that one’s professional identity should be closely aligned with their personal identity since it leads to authenticity in their work life and, eventually, job satisfaction.</p>
+        <br/>
+        <p>She therefore strives to help students identify their genuine interests and match these with their long term goals, equipping them with the mindset for success. Her personalised counselling sessions are creatively designed to ensure that every student enjoys discovering themselves. Using this approach, she has supported hundreds of students take a significant step towards their professional goals by placing them into the colleges of their dreams.</p>
+        <br/>
+        <p>In her spare time, she consumes and recommends books, articles, documentaries, movies, and theatre to her students to advance their interests and to build lifelong critical thinking habits and empathy. She also practises Vipassana meditation to develop mindfulness, and writes life narratives.</p>
+        <br/>
+      </>
+    ),
   };
 
   return (
